f6a5993c929bf2b0165797a3e40874d79aaf7ba6
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-06 22:14:01 +0900

    feat: Apollo를 이용해 데이터 받아오기

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,1 +1,29 @@
-export default () => <div>Home</div>;
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+    {
+        movies {
+            id
+            medium_cover_image
+        }
+    }
+`;
+
+export default () => {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+
+    if (loading) {
+        return "loading...";
+    }
+    if (data && data.movies) {
+        return data.movies.map(
+            (movie: { id: number; medium_cover_image: string }) => (
+                <div key={movie.id}>
+                    <img src={movie.medium_cover_image} />
+                </div>
+            )
+        );
+    }
+};
