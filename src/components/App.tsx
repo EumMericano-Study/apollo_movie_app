9fca904e442eaf15048e3f876b34acfb01965e55
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-06 21:31:13 +0900

    chore: router 초기 설정

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
 
 function App() {
-    return <div>hi</div>;
+    return (
+        <Router>
+            <Route path="/" element={<Home />} />
+            <Route path="/:id" element={<Detail />} />
+        </Router>
+    );
 }
 
 export default App;
