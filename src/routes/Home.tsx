c97f9ee65263b9ddc4ec05f9e95c7e00dfe8fdfa
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-06 22:36:33 +0900

    feat: typed styled-component 인스톨

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 
+import {
+    Container,
+    Loading,
+    Header,
+    Title,
+    Subtitle,
+    Movies,
+} from "./Home.style";
+
 const GET_MOVIES = gql`
     {
         movies {
@@ -13,17 +22,23 @@ const GET_MOVIES = gql`
 
 export default () => {
     const { loading, error, data } = useQuery(GET_MOVIES);
-
-    if (loading) {
-        return "loading...";
-    }
-    if (data && data.movies) {
-        return data.movies.map(
-            (movie: { id: number; medium_cover_image: string }) => (
-                <div key={movie.id}>
-                    <img src={movie.medium_cover_image} />
-                </div>
-            )
-        );
-    }
+    return (
+        <Container>
+            <Header>
+                <Title>Movie App with React and Apollo</Title>
+                <Subtitle>and GraphQL</Subtitle>
+            </Header>
+            <Movies>
+                {data &&
+                    data.movies &&
+                    data.movies.map(
+                        (movie: { id: number; medium_cover_image: string }) => (
+                            <div key={movie.id}>
+                                <img src={movie.medium_cover_image} />
+                            </div>
+                        )
+                    )}
+            </Movies>
+        </Container>
+    );
 };
